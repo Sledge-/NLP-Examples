d900e25728e1e5cfbba65ba23daf166b2e3b6174
Karl <karl.schelhammer@gmail.com>
2019-01-15 09:45:20 -0500

    Updating data-info.txt

--- code-along/bow_classifier.py
+++ code-along/bow_classifier.py
@@ -12,4 +12,13 @@ from gensim.models import KeyedVectors
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from gensim.models import KeyedVectors
 
-train = pd.read_csv
+train = pd.read_csv('../umb_textmining/r8-train-all-terms.txt', header=None, sep='\t')
+test = pd.read_csv('../umb_textmining/r8-test-all-terms.txt', header=None, sep='\t')
+train.columns = ['label', 'content']
+test.columns = ['label', 'content']
+
+
+class GloveVectorizer:
+    def __init__(self):
+        print('Loading word vectors...')
+        word2vec = {}
