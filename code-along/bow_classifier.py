f3e7ae9a60d6ffc3d8d55ea95624efb92d1f1e52
Karl <karl.schelhammer@gmail.com>
2019-01-15 09:19:17 -0500

    Updating BOW Classifier.

---
+++ code-along/bow_classifier.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, division
+from builtins import range
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from gensim.models import KeyedVectors
+
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from gensim.models import KeyedVectors
+
+train = pd.read_csv
